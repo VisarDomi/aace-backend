77c586f4c4d96402af71b9784118480586a167d8
ErdalDomi <erdal.domi2@gmail.com>
2018-12-16 21:58:33 +0100

    importing flusk api content

--- aace-backend/api/__init__.py
+++ aace-backend/api/__init__.py
@@ -1,3 +1,38 @@
-from flask import Flask 
+from flask import Flask
 
-app = Flask(__name__)
+from .common.database import init_db
+from .common.middleware import after_request_middleware, before_request_middleware, teardown_appcontext_middleware
+from .common.middleware import response
+from .foss import bp as foss_bp
+
+from flask_cors import CORS
+
+
+
+def create_app():
+
+    # initialize flask application
+    app = Flask(__name__)
+    CORS(app)
+    # register all blueprints
+    app.register_blueprint(foss_bp)
+
+    # register custom response class
+    app.response_class = response.JSONResponse
+
+    # register before request middleware
+    before_request_middleware(app=app)
+
+    # register after request middleware
+    after_request_middleware(app=app)
+
+    # register after app context teardown middleware
+    teardown_appcontext_middleware(app=app)
+
+    # register custom error handler
+    response.json_error_handler(app=app)
+
+    # initialize the database
+    init_db()
+
+    return app
