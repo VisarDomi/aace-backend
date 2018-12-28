400380c35f316d247f78848bc9fd447ccccaa760
Visar Domi <visardomi4@gmail.com>
2018-12-29 00:45:39 +0100

    fixed sintax bug, Class to class

--- aace-backend/config.py
+++ aace-backend/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.flaskenv'))
 
-Class Config(object):
+class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess1'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
