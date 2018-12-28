e661bca524ded2605f83c97557225072b54ec464
Visar Domi <visardomi4@gmail.com>
2018-12-29 00:41:13 +0100

    "somewhat working config env"

--- aace-backend/api/__init__.py
+++ aace-backend/api/__init__.py
@@ -14,12 +14,18 @@ from .bp_notification import bp as notification_bp
 from .bp_post import bp as post_bp
 from .bp_user import bp as user_bp
 
+from config import Config
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 
 def create_app():
 
     # initialize flask application
     app = Flask(__name__)
+
+    # config app
+    app.config.from_object(Config)
 
     # register all blueprints
     app.register_blueprint(auth_bp)
@@ -51,5 +57,32 @@ def create_app():
     # initialize the database
     # drop_db()
     init_db()
+
+    if not app.debug and not app.testing:
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMINS'], subject='AACE-Network Failure',
+                credentials=auth, secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/aace-network.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('AACE-Network')
 
     return app
