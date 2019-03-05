b09482186dc5510d1ede9b0aee090880120474cf
Visar Domi <visardomi4@gmail.com>
2019-03-06 00:39:52 +0100

    updated search to be case insensitive and accept partial names

--- api/bp_search/backend.py
+++ api/bp_search/backend.py
@@ -2,11 +2,13 @@ from ..common.models import User
 
 
 def search_users(search_user_data):
-    print("search_user_data['fname'] :", search_user_data["fname"])
     fname = search_user_data["fname"]
     lname = search_user_data["lname"]
+    looking_for_fname = '%{0}%'.format(fname)
+    looking_for_lname = '%{0}%'.format(lname)
     users = User.query.filter(
-        User.first_name.contains(fname) & User.last_name.contains(lname)
+        User.first_name.ilike(looking_for_fname)
+        & User.last_name.ilike(looking_for_lname)
     ).all()
 
     return users
