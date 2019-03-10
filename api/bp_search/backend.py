f8268e62dff0fb85cdb9ceb729fe4faacaba16c9
Visar Domi <visardomi4@gmail.com>
2019-03-10 02:09:15 +0100

    updated search to get only name, and then split it serverside

--- api/bp_search/backend.py
+++ api/bp_search/backend.py
@@ -3,19 +3,30 @@ from ..common.exceptions import NotFound
 
 
 def search_users(search_user_data):
-    print("search_user_data: ", search_user_data)
+    fname = None
+    lname = None
+    users = []
     try:
-        fname = search_user_data["fname"]
-        lname = search_user_data["lname"]
+        name = search_user_data["name"]
     except KeyError:
         msg = f"The keys fname and lname are not found."
         raise NotFound(message=msg)
+    names = name.split(" ")
+    if len(names) == 1:
+        fname = names.pop(0)
+    elif len(names) == 2:
+        fname = names.pop(0)
+        lname = names.pop(0)
+
     looking_for_fname = "%{0}%".format(fname)
-    looking_for_lname = "%{0}%".format(lname)
-    users = User.query.filter(
-        User.first_name.ilike(looking_for_fname)
-        & User.last_name.ilike(looking_for_lname)
-    ).all()
-    # users=[]
+    if fname and not lname:
+        looking_for_lname = "%{0}%".format(lname)
+        users = User.query.filter(User.first_name.ilike(looking_for_fname)).all()
+    elif fname and lname:
+        looking_for_lname = "%{0}%".format(lname)
+        users = User.query.filter(
+            User.first_name.ilike(looking_for_fname)
+            & User.last_name.ilike(looking_for_lname)
+        ).all()
 
     return users
