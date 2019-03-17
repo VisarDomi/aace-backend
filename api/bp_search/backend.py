d1854894ce3563080cb7e3c1aa4ca265039a6979
Visar Domi <visardomi4@gmail.com>
2019-03-17 02:38:13 +0100

    last name search works now

--- api/bp_search/backend.py
+++ api/bp_search/backend.py
@@ -3,13 +3,15 @@ from ..common.exceptions import NotFound
 
 
 def search_users(search_user_data):
-    fname = None
-    lname = None
+    print("search_user_data :", search_user_data)
+    first_name = None
+    last_name = None
     users = []
     try:
         name = search_user_data["name"]
+        print("name :", name)
     except KeyError:
-        msg = f"The keys fname and lname are not found."
+        msg = f"The keys first_name and last_name are not found."
         raise NotFound(message=msg)
     accepted_members_query = User.query.filter(User.register_status == "accepted")
 
@@ -17,22 +19,30 @@ def search_users(search_user_data):
         users = accepted_members_query.all()
     names = name.split(" ")
     if len(names) == 1:
-        fname = names.pop(0)
+        first_name = names.pop(0)
+        print("first_name :", first_name)
     elif len(names) == 2:
-        fname = names.pop(0)
-        lname = names.pop(0)
+        first_name = names.pop(0)
+        last_name = names.pop(0)
 
-    looking_for_fname = "%{0}%".format(fname)
-    if fname and not lname:
-        looking_for_lname = "%{0}%".format(lname)
+    looking_for_first_name = "%{0}%".format(first_name)
+    looking_for_last_name = "%{0}%".format(last_name)
+    print("looking_for_first_name :", looking_for_first_name)
+    print("looking_for_last_name :", looking_for_last_name)
+    if first_name and not last_name:
+        fname = first_name
+        lname = first_name
+        looking_for_first_name = "%{0}%".format(fname)
+        looking_for_last_name = "%{0}%".format(lname)
         users = accepted_members_query.filter(
-            User.first_name.ilike(looking_for_fname)
+            (User.first_name.ilike(looking_for_first_name))
+            | (User.last_name.ilike(looking_for_last_name))
         ).all()
-    elif fname and lname:
-        looking_for_lname = "%{0}%".format(lname)
+    elif first_name and last_name:
+        looking_for_last_name = "%{0}%".format(last_name)
         users = accepted_members_query.filter(
-            User.first_name.ilike(looking_for_fname)
-            & User.last_name.ilike(looking_for_lname)
+            (User.first_name.ilike(looking_for_first_name))
+            & (User.last_name.ilike(looking_for_last_name))
         ).all()
 
     return users
